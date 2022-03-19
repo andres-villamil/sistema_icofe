0a8d80b02884abb99c65097bceef01525ac131d2
davillamilm <davillamilm@dane.gov.co>
2022-03-19 12:39:24 -0500

    pruebas unitarias

--- src/herramientaPlanificacion/consumidoresInfo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConsumidoresinfoConfig(AppConfig):
-    name = 'consumidoresInfo'
---
+++ src/herramientaPlanificacion/test/test_login.py
@@ -0,0 +1,28 @@
+import pytest
+from login.models import User
+
+@pytest.mark.django_db
+def test_user_creation():
+    user = User.objects.create_user(
+        username = 'pruebaBanco',
+        email = 'andres7villa24@gmail.com',
+        password = '1234qpl5678#*',
+        first_name = 'Diego'
+
+    )
+
+    assert user.username  == "pruebaBanco"
+
+
+@pytest.mark.django_db
+def test_staff_user_creation():
+    user = User.objects.create_user(
+        username = 'pruebaBanco',
+        email = 'andres7villa24@gmail.com',
+        password = '1234qpl5678#*',
+        first_name = 'Diego',
+        is_staff = True
+
+    )
+
+    assert user.is_staff 
